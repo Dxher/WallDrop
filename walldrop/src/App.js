f23eadce05813457c1af26ddf912af3cdbc643a3
tony daher <tonydaher@hotmail.ca>
2023-07-21 12:32:53 -0400

    attempting to fix page loading issue

--- walldrop/src/App.js
+++ walldrop/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomNavbar from './components/CustomNavbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,22 +9,20 @@ import Join from './components/Join';
 import Upload from './components/Upload';
 import WallpaperDetails from './components/WallpaperDetails';
 
-
 const App = () => {
-    
   return (
     <Router>
       <div className="App">
         <CustomNavbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/wallpapers/:id" element={<WallpaperDetails />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/wallpapers/:id" element={<WallpaperDetails />} />
+        </Routes>
       </div>
     </Router>
   );
